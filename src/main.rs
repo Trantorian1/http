9dd39f56ecb55dd08454402a88b0ff17edf40971
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-07-27 12:34:58 +0200

    feat(http): status code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
-#[allow(unused_imports)]
+use std::io::Write as _;
 use std::net::TcpListener;
+use tracing_subscriber::prelude::*;
+
+const ADDRESS: &str = "127.0.0.1:4221";
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let level = tracing::level_filters::LevelFilter::INFO;
+    let env = tracing_subscriber::EnvFilter::builder()
+        .with_default_directive(level.into())
+        .from_env_lossy();
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(env)
+        .init();
+
+    let listener = TcpListener::bind(ADDRESS).unwrap();
+    tracing::info!("Listening on {ADDRESS}");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                tracing::trace!("accepted new connection");
+
+                if let Err(err) = stream.write_all("HTTP/1.1 200 OK \r\n".as_bytes()) {
+                    tracing::error!("Failed to send data back to TPC stream: {err}");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
