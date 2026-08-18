33b1de922aaac6b8691e39092187f5a15d8ab4fc
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-08-18 17:29:32 +0200

    docs(http): updated docs

--- http_1/src/server/mod.rs
+++ http_1/src/server/mod.rs
@@ -1,21 +1,30 @@
 //! HTTP/1.1 server implementation.
 //!
-//! [`Server`]s are grouped by instances, with each instance responsible for handling it's own
-//! stack-allocated memory [`Buffer`]. This makes it possible to spawn multiple instances, for
-//! example across multiple threads, in order to support concurrent request handling without any
-//! form of locking or memory contention.
+//! [`Server`]s are implemented as stateless message handlers, with each instance responsible for
+//! it's own stack-allocated memory [`Buffer`]. This makes it easy to spawn instances across
+//! multiple threads in order to support concurrent request handling without any form of locking or
+//! memory contention.
 
 use crate::prelude::*;
 use http_core::prelude::*;
 
 /// HTTP/1.1 server instance, handles connection responses and ensures proper flushing between
 /// requests.
+///
+/// Note that servers only handle message responses. Request must instead be fed manually from a
+/// separate [`TcpListener`] using [`process`].
+///
+/// [`TcpListener`]: std::net::TcpListener
+/// [`process`]: Self::process
 pub struct Server<'data> {
     global_request_buffer: BufferForReading<'data>,
     global_response_buffer: BufferForWriting<'data>,
 }
 
 impl<'data> Server<'data> {
+    /// Creates a new server instance.
+    ///
+    /// [`TcpListener`]: std::net::TcpListener
     pub fn new(
         global_request_buffer: &'data mut [u8],
         global_response_buffer: &'data mut [u8],
@@ -57,31 +66,6 @@ where
     RW: std::io::Read + std::io::Write,
 {
     /// Respond to a TCP [`Request`].
-    ///
-    /// ```rust
-    /// # use http_core::prelude::*;
-    /// # use http_1::prelude::*;
-    /// #
-    /// # use std::io::Write as _;
-    /// #
-    /// # let mut stream_buffer = [0; 8 * KB];
-    /// # let mut stream = ByteStream::new(&mut stream_buffer);
-    /// # stream.write(b"GET / HTTP/1.1\r\n\r\n").unwrap();
-    /// #
-    /// # let mut global_request_buffer = [0; 8 * KB];
-    /// # let mut global_response_buffer = [0; 64 * KB];
-    /// # let mut server = Server::new(&mut global_request_buffer, &mut global_response_buffer);
-    /// server
-    ///     .process(stream)
-    ///     .respond(|request, response| match request.target {
-    ///         b"/" => response
-    ///             .with_status_code(Status::Ok)
-    ///             .send(),
-    ///         _ => response
-    ///             .with_status_code(Status::NotFound)
-    ///             .send(),
-    ///     });
-    /// ```
     pub fn respond(
         mut self,
         f: fn(RequestInfo<'buf>, Response<'buf, 'data, '_, RW>) -> std::io::Result<()>,
