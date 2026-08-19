392de00b73364530685e9379f87d2389f99aaf18
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-08-19 13:08:50 +0200

    fix(stream): offset bugs

--- http_primitives/src/mem/stream/mod.rs
+++ http_primitives/src/mem/stream/mod.rs
@@ -23,7 +23,7 @@ use crate::prelude::*;
 /// ```
 ///
 /// [`Read`]: std::io::Read
-/// [`Write`]: std::io::Write
+// [`Write`]: std::io::Write
 /// [`TcpStream`]: std::net::TcpStream
 pub struct ByteStream<'data> {
     buffer: &'data mut [u8],
@@ -32,17 +32,48 @@ pub struct ByteStream<'data> {
 }
 
 impl<'data> ByteStream<'data> {
-    /// Creates a new by stream. Will panic if `SIZE` is equal to 0.
+    /// Creates a new [`ByteStream`]. Will panic if `buffer` is empty.
     pub fn new(buffer: &'data mut [u8]) -> Self {
+        Self::new_with_offset(buffer, 0)
+    }
+
+    fn new_with_offset(buffer: &'data mut [u8], offset: usize) -> Self {
         assert!(!buffer.is_empty());
+        assert_le!(offset, buffer.len());
 
         // Zero-away the buffer to guard against misuse
         buffer.fill(0);
 
         Self {
             buffer,
-            start: 0,
+            start: offset,
             size: 0,
+        }
+    }
+
+    /// Creates a new [`ByteStream`], using `buffer` as initialized memory. This can be useful in
+    /// testing for example.
+    ///
+    /// Will panic if `buffer` is empty.
+    ///
+    /// ```rust
+    /// # use std::io::Read as _;
+    /// # use http_primitives::prelude::*;
+    /// let mut array: [u8; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
+    /// let mut stream = ByteStream::pre_populate(&mut array);
+    ///
+    /// let read_buffer = [0; 8];
+    /// let bytes = stream.read(&mut read_buffer).unwrap();
+    ///
+    /// assert_eq!(&read_buffer[..bytes], &[0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    /// ```
+    pub fn pre_populate(buffer: &'data mut [u8]) -> Self {
+        assert!(!buffer.is_empty());
+
+        Self {
+            start: 0,
+            size: buffer.len(),
+            buffer,
         }
     }
 
@@ -60,6 +91,13 @@ impl<'data> ByteStream<'data> {
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
+
+    /// Returns an iterator over the stream.
+    ///
+    /// The iterator yields all items from start to end, consuming them as it goes.
+    pub fn iter(&mut self) -> Iter<'_, 'data> {
+        Iter::new(self)
+    }
 }
 
 impl<'data> std::io::Read for ByteStream<'data> {
@@ -73,7 +111,7 @@ impl<'data> std::io::Read for ByteStream<'data> {
 
         if stop <= self.capacity() {
             // Stream data stops before the end of the buffer, no need for wrap-around logic.
-            let space_after_start = stop.min(bytes);
+            let space_after_start = self.size.min(bytes);
 
             // Single-copy, retrieve all data before the end of the buffer.
             buf[..space_after_start]
@@ -98,7 +136,7 @@ impl<'data> std::io::Read for ByteStream<'data> {
                 .copy_from_slice(&self.buffer[..space_before_stop]);
 
             self.start = (self.start + space_after_start + space_before_stop) % self.capacity();
-            self.size -= space_after_start - space_before_stop;
+            self.size -= space_after_start + space_before_stop;
 
             Ok(space_after_start + space_before_stop)
         }
@@ -120,7 +158,7 @@ impl<'data> std::io::Write for ByteStream<'data> {
 
         self.buffer[stop..stop + space_after_stop].copy_from_slice(&buf[..space_after_stop]);
 
-        if stop <= self.capacity() {
+        if start + bytes <= self.capacity() {
             // No wrap-around needed.
             self.size += space_after_stop;
 
@@ -135,12 +173,49 @@ impl<'data> std::io::Write for ByteStream<'data> {
 
             self.size += space_after_stop + space_before_start;
 
-            Ok(space_after_stop - space_before_start)
+            Ok(space_after_stop + space_before_start)
         }
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
         Ok(())
+    }
+}
+
+pub struct Iter<'stream, 'data>
+where
+    'data: 'stream,
+{
+    stream: &'stream mut ByteStream<'data>,
+}
+
+impl<'stream, 'data> Iter<'stream, 'data> {
+    pub fn new(stream: &'stream mut ByteStream<'data>) -> Self {
+        Self { stream }
+    }
+}
+
+impl<'stream, 'data> Iterator for Iter<'stream, 'data> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.stream.size > 0 {
+            let index = self.stream.start;
+            let index_corrected = if index >= self.stream.capacity() {
+                index - self.stream.capacity()
+            } else {
+                index
+            };
+
+            let item = self.stream.buffer[index_corrected];
+
+            self.stream.start += 1;
+            self.stream.size -= 1;
+
+            Some(item)
+        } else {
+            None
+        }
     }
 }
 
@@ -153,33 +228,38 @@ pub mod test {
 
     const SIZE: usize = 16;
 
+    #[rstest::fixture]
+    fn array() -> [u8; SIZE] {
+        std::array::from_fn(|i| i as u8)
+    }
+
     #[test]
     fn stream_init() {
         let mut stream_buffer = [0; SIZE];
-        let mut stream = ByteStream::new(&mut stream_buffer);
-
-        let mut buffer = [0; SIZE];
+        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 0);
 
         assert_eq!(stream.len(), 0, "An empty stream must be empty");
         assert_eq!(stream.start, 0, "An empty stream must start at index 0");
 
+        let mut read_buffer = [0; SIZE];
         let bytes = stream
-            .read(&mut buffer)
+            .read(&mut read_buffer)
             .expect("Reading a byte stream must always succeed");
 
         assert_eq!(bytes, 0, "An empty stream must not contain any data");
-        assert_eq!(buffer, [0; SIZE], "Reading empty stream has no side effect");
+        assert_eq!(
+            read_buffer, [0; SIZE],
+            "Reading empty stream has no side effect"
+        );
     }
 
     #[test]
     fn stream_read_write() {
         let mut stream_buffer = [0; SIZE];
-        let mut stream = ByteStream::new(&mut stream_buffer);
-
-        let mut buffer = [0; SIZE];
+        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 0);
 
         let message = b"Hello, World";
-        assert_le!(message.len(), SIZE, "Message must fit in the stream");
+        assert_leq!(message.len(), SIZE, "Message must fit in the stream");
 
         let bytes = stream
             .write(message)
@@ -189,14 +269,85 @@ pub mod test {
         assert_eq!(stream.len(), message.len(), "Stream length must update");
         assert_eq!(stream.start, 0, "The stream's start index must not change");
 
+        let mut read_buffer = [0; SIZE];
         let bytes = stream
-            .read(&mut buffer)
-            .expect("Read on a data stream should always succeed");
+            .read(&mut read_buffer)
+            .expect("Read on a byte stream should always succeed");
 
         assert_eq!(bytes, message.len(), "All message bytes must be read");
-        assert_eq!(&buffer[..bytes], message, "Buffer should contain message");
+        assert_eq!(
+            &read_buffer[..bytes],
+            message,
+            "Buffer should contain message"
+        );
+
         assert_eq!(stream.len(), 0, "Stream reads must consume the data read");
         assert_eq!(stream.start, message.len(), "Stream reads update start idx");
+    }
+
+    #[test]
+    fn stream_read_offset() {
+        let mut stream_buffer = [0; 2];
+        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 1);
+
+        let message = b"hi";
+        assert_leq!(message.len(), 2, "Message must fit in the stream");
+
+        let bytes = stream
+            .write(message)
+            .expect("Writing to a byte stream of sufficient size must succeed");
+
+        assert_eq!(bytes, message.len(), "All message bytes must be written");
+        assert_eq!(stream.len(), message.len(), "Stream length must update");
+        assert_eq!(stream.start, 1, "The stream's start index must not change");
+
+        let mut read_buffer = [0; 2];
+        let bytes = stream
+            .read(&mut read_buffer)
+            .expect("Read on a byte stream should always succeed");
+
+        assert_eq!(bytes, message.len(), "All message bytes must be read");
+        assert_eq!(
+            &read_buffer[..bytes],
+            message,
+            "Buffer should contain message"
+        );
+
+        assert_eq!(stream.len(), 0, "Stream reads must consume the data read");
+        assert_eq!(stream.start, 1, "Stream reads update start idx");
+    }
+
+    #[test]
+    fn stream_read_empty() {
+        let mut stream_buffer = [0; 2];
+        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 0);
+        assert!(stream.is_empty(), "Stream must be empty");
+
+        let mut read_buffer = [0; 2];
+        let bytes = stream
+            .read(&mut read_buffer)
+            .expect("Read on byte stream should always succeed");
+
+        assert_eq!(bytes, 0, "No bytes should have been read off empty stream");
+        assert!(stream.is_empty(), "Stream should still be emtpy");
+        assert_eq!(stream.start, 0, "Reading emtpy stream musn't mutate start");
+    }
+
+    #[test]
+    fn stream_read_empty_with_offset() {
+        let mut stream_buffer = [0; 2];
+        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 1);
+        assert!(stream.is_empty(), "Stream must be empty");
+        assert_eq!(stream.start, 1, "Stream offset must be set");
+
+        let mut read_buffer = [0; 2];
+        let bytes = stream
+            .read(&mut read_buffer)
+            .expect("Read on byte stream should always succeed");
+
+        assert_eq!(bytes, 0, "No bytes should have been read off empty stream");
+        assert!(stream.is_empty(), "Stream should still be emtpy");
+        assert_eq!(stream.start, 1, "Reading emtpy stream musn't mutate start");
     }
 
     #[test]
@@ -270,8 +421,6 @@ mod validate {
     #[cfg_attr(kani, kani::unwind(17))]
     fn stream_harness() {
         let generator = (
-            // Random byte buffer used for writing
-            bolero::produce::<[u8; MAX_SIZE]>(),
             // Number of bytes to write
             bolero::produce::<usize>().with().bounds(..MAX_SIZE),
             // Stream capacity, cannot be 0
@@ -280,12 +429,21 @@ mod validate {
 
         bolero::check!()
             .with_generator(generator)
+            .and_then(|(n, size)| {
+                (
+                    n,
+                    size,
+                    // Stream start offset
+                    bolero::produce::<usize>().with().bounds(..size),
+                )
+            })
             .cloned()
-            .for_each(|(bytes, n, size)| {
-                let mut backing = [0u8; MAX_SIZE];
-                let mut read_buffer = [0u8; MAX_SIZE];
+            .for_each(|(n, size, offset)| {
+                let bytes = [1; MAX_SIZE];
+                let mut backing = [0; MAX_SIZE];
+                let mut read_buffer = [0; MAX_SIZE];
 
-                let mut stream = ByteStream::new(&mut backing[..size]);
+                let mut stream = ByteStream::new_with_offset(&mut backing[..size], offset);
 
                 let written = stream.write(&bytes[..n]).unwrap();
                 assert_eq!(written, n.min(size));
@@ -294,5 +452,42 @@ mod validate {
                 assert_eq!(read, written);
                 assert_eq!(&read_buffer[..read], &bytes[..read]);
             })
+    }
+
+    #[test]
+    #[cfg_attr(kani, kani::proof)]
+    #[cfg_attr(kani, kani::unwind(17))]
+    fn stream_iter() {
+        let generator = (
+            // Number of bytes to write
+            bolero::produce::<usize>().with().bounds(..MAX_SIZE),
+            // Stream capacity, cannot be 0
+            bolero::produce::<usize>().with().bounds(1..MAX_SIZE),
+        );
+
+        bolero::check!()
+            .with_generator(generator)
+            .and_then(|(n, size)| {
+                (
+                    n,
+                    size,
+                    // Stream start offset
+                    bolero::produce::<usize>().with().bounds(..size),
+                )
+            })
+            .cloned()
+            .for_each(|(n, size, offset)| {
+                let bytes = [1; MAX_SIZE];
+                let mut backing = [0; MAX_SIZE];
+                let mut stream = ByteStream::new_with_offset(&mut backing[..size], offset);
+
+                let written = stream.write(&bytes[..n]).unwrap();
+                assert_eq!(written, n.min(size));
+
+                let mut iter = stream.iter();
+                for n in &bytes[..written] {
+                    assert_eq!(Some(*n), iter.next());
+                }
+            });
     }
 }
