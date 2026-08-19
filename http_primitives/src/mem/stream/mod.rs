722870735cd9e0c0e93995295e58654a955d488c
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-08-19 20:07:36 +0200

    teat(stream): function contracts

--- http_primitives/src/mem/stream/mod.rs
+++ http_primitives/src/mem/stream/mod.rs
@@ -34,21 +34,10 @@ pub struct ByteStream<'data> {
 impl<'data> ByteStream<'data> {
     /// Creates a new [`ByteStream`]. Will panic if `buffer` is empty.
     pub fn new(buffer: &'data mut [u8]) -> Self {
-        Self::new_with_offset(buffer, 0)
-    }
-
-    fn new_with_offset(buffer: &'data mut [u8], offset: usize) -> Self {
-        assert!(!buffer.is_empty());
-        assert_le!(offset, buffer.len());
-
         // Zero-away the buffer to guard against misuse
         buffer.fill(0);
 
-        Self {
-            buffer,
-            start: offset,
-            size: 0,
-        }
+        Self::any(buffer, 0, 0)
     }
 
     /// Creates a new [`ByteStream`], using `buffer` as initialized memory. This can be useful in
@@ -68,12 +57,18 @@ impl<'data> ByteStream<'data> {
     /// assert_eq!(&read_buffer[..bytes], &[0, 1, 2, 3, 4, 5, 6, 7]);
     /// ```
     pub fn pre_populate(buffer: &'data mut [u8]) -> Self {
+        Self::any(buffer, 0, buffer.len())
+    }
+
+    fn any(buffer: &'data mut [u8], start: usize, size: usize) -> Self {
         assert!(!buffer.is_empty());
+        assert_le!(start, buffer.len());
+        assert_leq!(start, buffer.len());
 
         Self {
-            start: 0,
-            size: buffer.len(),
             buffer,
+            start,
+            size,
         }
     }
 
@@ -92,6 +87,11 @@ impl<'data> ByteStream<'data> {
         self.len() == 0
     }
 
+    /// Returns the amount of space in the stream which can still be written to.
+    pub fn space_left(&self) -> usize {
+        self.capacity() - self.size
+    }
+
     /// Returns an iterator over the stream.
     ///
     /// The iterator yields all items from start to end, consuming them as it goes.
@@ -101,6 +101,36 @@ impl<'data> ByteStream<'data> {
 }
 
 impl<'data> std::io::Read for ByteStream<'data> {
+    //
+    // -- Mutations
+    //
+    #[cfg_attr(kani, kani::modifies(&self.start, &self.size, buf))]
+    //
+    // -- Pre-conditions
+    //
+    #[cfg_attr(kani, kani::requires(contracts::common_preconditions(self)))]
+    //
+    // -- Post-conditions
+    //
+    // Start index must wrap around the buffer.
+    //
+    #[cfg_attr(kani, kani::ensures(|_| self.start == (old(self.start) + old(self.size).min(buf.len())) % self.capacity()))]
+    //
+    // Bytes are consumed as they are read.
+    //
+    #[cfg_attr(kani, kani::ensures(|_| self.size == old(self.size) - old(self.size).min(buf.len())))]
+    //
+    // Results are coherent with the data being read and can never error out.
+    //
+    #[cfg_attr(kani, kani::ensures(|result| match result  {
+        Ok(read) => *read == old(self.size).min(buf.len())
+            && self.size == old(self.size) - *read,
+        Err(_) => false
+    }))]
+    ///
+    /// If this seems confusing to you, check out the `kani` docs on [function contracts]
+    ///
+    /// [function contracts]: https://model-checking.github.io/kani/crates/doc/kani/contracts/index.html
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         assert_leq!(self.start, self.capacity());
         assert_leq!(self.size, self.capacity());
@@ -117,7 +147,7 @@ impl<'data> std::io::Read for ByteStream<'data> {
             buf[..space_after_start]
                 .copy_from_slice(&self.buffer[start..start + space_after_start]);
 
-            self.start += space_after_start;
+            self.start = (self.start + space_after_start) % self.capacity();
             self.size -= space_after_start;
             assert_leq!(self.size, self.capacity());
 
@@ -146,6 +176,36 @@ impl<'data> std::io::Read for ByteStream<'data> {
 }
 
 impl<'data> std::io::Write for ByteStream<'data> {
+    //
+    // -- Mutations
+    //
+    #[cfg_attr(kani, kani::modifies(&self.start, &self.size, self.buffer))]
+    //
+    // -- Pre-conditions
+    //
+    #[cfg_attr(kani, kani::requires(contracts::common_preconditions(self)))]
+    //
+    // -- Post-conditions
+    //
+    // Start index cannot be mutated by stream writes, only stream reads.
+    //
+    #[cfg_attr(kani, kani::ensures(|_| self.start == old(self.start)))]
+    //
+    // Stream size grows with the number of bytes written.
+    //
+    #[cfg_attr(kani, kani::ensures(|_| self.size == old(self.size) + old(self.space_left()).min(buf.len())))]
+    //
+    // Results are coherent with the data being written and can never error out.
+    //
+    #[cfg_attr(kani, kani::ensures(|result| match result {
+        Ok(written) => *written == old(self.space_left()).min(buf.len())
+            && self.size == old(self.size) + *written,
+        Err(_) => false
+    }))]
+    ///
+    /// If this seems confusing to you, check out the `kani` docs on [function contracts]
+    ///
+    /// [function contracts]: https://model-checking.github.io/kani/crates/doc/kani/contracts/index.html
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         assert_leq!(self.start, self.capacity());
         assert_leq!(self.size, self.capacity());
@@ -153,31 +213,36 @@ impl<'data> std::io::Write for ByteStream<'data> {
         let start = self.start;
         let stop = self.start + self.size;
         let bytes = buf.len();
-        let mut written = 0;
 
-        if stop < self.capacity() {
-            // Try and append to the inner buffer if there is space at the end
+        if stop <= self.capacity() {
+            // The data currently in the buffer is contiguous, we might need to wrap around in order
+            // to write more bytes. Here, were start by appending to the end of the stream.
             let space_after_stop = (self.capacity() - stop).min(bytes);
             self.buffer[stop..stop + space_after_stop].copy_from_slice(&buf[..space_after_stop]);
 
-            written += space_after_stop;
-            assert_leq!(written, bytes);
-        }
-
-        if start > 0 && written < bytes {
-            // Wrap around to the front if there is any space left
-            let space_before_start = start.min(bytes - written);
+            // Next, we try and write whatever bytes remain at the start of the stream.
+            let space_before_start = start.min(bytes - space_after_stop);
             self.buffer[..space_before_start]
-                .copy_from_slice(&buf[written..written + space_before_start]);
+                .copy_from_slice(&buf[space_after_stop..space_after_stop + space_before_start]);
 
-            written += space_before_start;
-            assert_leq!(written, bytes);
+            self.size += space_after_stop + space_before_start;
+
+            Ok(space_after_stop + space_before_start)
+        } else {
+            // The data currently in the buffer is NOT contiguous and wraps around. This actually
+            // makes our life easier, as we only need a single write to cover the area of memory
+            // which we have left.
+            let stop = stop - self.capacity();
+            let space_before_start = (start - stop).min(bytes);
+
+            // Write to the middle of the buffer, taking existing data wrap-around into consideration.
+            self.buffer[stop..stop + space_before_start]
+                .copy_from_slice(&buf[..space_before_start]);
+
+            self.size += space_before_start;
+
+            Ok(space_before_start)
         }
-
-        self.size += written;
-        assert_leq!(self.size, self.capacity());
-
-        Ok(written)
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
@@ -203,16 +268,9 @@ impl<'stream, 'data> Iterator for Iter<'stream, 'data> {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.stream.size > 0 {
-            let index = self.stream.start;
-            let index_corrected = if index >= self.stream.capacity() {
-                index - self.stream.capacity()
-            } else {
-                index
-            };
+            let item = self.stream.buffer[self.stream.start];
 
-            let item = self.stream.buffer[index_corrected];
-
-            self.stream.start += 1;
+            self.stream.start = (self.stream.start + 1) % self.stream.capacity();
             self.stream.size -= 1;
 
             Some(item)
@@ -239,7 +297,7 @@ pub mod test {
     #[test]
     fn stream_init() {
         let mut stream_buffer = [0; SIZE];
-        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 0);
+        let mut stream = ByteStream::new(&mut stream_buffer);
 
         assert_eq!(stream.len(), 0, "An empty stream must be empty");
         assert_eq!(stream.start, 0, "An empty stream must start at index 0");
@@ -259,7 +317,7 @@ pub mod test {
     #[test]
     fn stream_read_write() {
         let mut stream_buffer = [0; SIZE];
-        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 0);
+        let mut stream = ByteStream::new(&mut stream_buffer);
 
         let message = b"Hello, World";
         assert_leq!(message.len(), SIZE, "Message must fit in the stream");
@@ -291,7 +349,7 @@ pub mod test {
     #[test]
     fn stream_read_write_offset() {
         let mut stream_buffer = [0; 2];
-        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 1);
+        let mut stream = ByteStream::any(&mut stream_buffer, 1, 0);
 
         let message = b"hi";
         assert_leq!(message.len(), 2, "Message must fit in the stream");
@@ -323,7 +381,7 @@ pub mod test {
     #[test]
     fn stream_read_empty() {
         let mut stream_buffer = [0; 2];
-        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 0);
+        let mut stream = ByteStream::any(&mut stream_buffer, 0, 0);
         assert!(stream.is_empty(), "Stream must be empty");
 
         let mut read_buffer = [0; 2];
@@ -339,7 +397,7 @@ pub mod test {
     #[test]
     fn stream_read_empty_with_offset() {
         let mut stream_buffer = [0; 2];
-        let mut stream = ByteStream::new_with_offset(&mut stream_buffer, 1);
+        let mut stream = ByteStream::any(&mut stream_buffer, 1, 0);
         assert!(stream.is_empty(), "Stream must be empty");
         assert_eq!(stream.start, 1, "Stream offset must be set");
 
@@ -351,6 +409,21 @@ pub mod test {
         assert_eq!(bytes, 0, "No bytes should have been read off empty stream");
         assert!(stream.is_empty(), "Stream should still be emtpy");
         assert_eq!(stream.start, 1, "Reading emtpy stream musn't mutate start");
+    }
+
+    #[rstest::rstest]
+    fn stream_read_empty_buffer(mut array: [u8; SIZE], #[from(array)] oracle: [u8; SIZE]) {
+        let mut stream = ByteStream::any(&mut array, 0, SIZE);
+        assert_eq!(stream.len(), oracle.len(), "Stream is not empty");
+
+        let mut read_buffer = [0; 0];
+        let bytes = stream
+            .read(&mut read_buffer)
+            .expect("Read on byte stream should always succeed");
+
+        assert_eq!(bytes, 0, "No bytes can be read with an empty read buffer");
+        assert_eq!(stream.len(), oracle.len(), "Stream must not have been read");
+        assert_eq!(stream.start, 0, "Empty buffer read musn't mutate start");
     }
 
     #[test]
@@ -433,31 +506,63 @@ mod validate {
 
         bolero::check!()
             .with_generator(generator)
-            .and_then(|(n, size)| {
+            .and_then(|(n, capacity)| {
                 (
                     n,
-                    size,
-                    // Stream start offset
-                    bolero::produce::<usize>().with().bounds(..size),
+                    capacity,
+                    // Initial stream start index
+                    bolero::produce::<usize>().with().bounds(..capacity),
+                    // Initial stream size
+                    bolero::produce::<usize>().with().bounds(..capacity),
                 )
             })
             .cloned()
-            .for_each(|(n, size, offset)| {
-                let bytes: [u8; MAX_SIZE] = std::array::from_fn(|i| i as u8);
-                let mut backing = [0; MAX_SIZE];
+            .for_each(|(n, capacity, start, size)| {
+                let mut backing: [u8; MAX_SIZE] = std::array::from_fn(|i| i as u8);
                 let mut read_buffer = [0; MAX_SIZE];
 
-                let mut stream = ByteStream::new_with_offset(&mut backing[..size], offset);
+                let bytes_prev = backing;
+                let bytes_new: [u8; MAX_SIZE] = std::array::from_fn(|i| (i + MAX_SIZE) as u8);
 
-                let written = stream.write(&bytes[..n]).unwrap();
-                assert_eq!(written, n.min(size));
+                let mut stream = ByteStream::any(&mut backing[..capacity], start, size);
 
-                let read = stream.read(&mut read_buffer[..]).unwrap();
-                assert_eq!(read, written);
-                assert_eq!(&read_buffer[..read], &bytes[..read]);
+                let written = stream.write(&bytes_new[..n]).unwrap();
+                assert_eq!(written, n.min(capacity - size));
+
+                let read = stream.read(&mut read_buffer).unwrap();
+
+                assert_eq!(read, written + size);
+                assert!(stream.is_empty());
+
+                // Make sure that previous data has not been overwritten
+                for i in 0..size {
+                    assert_eq!(read_buffer[i], bytes_prev[(i + start) % capacity]);
+                }
+
+                // Check that new data has been written correctly
+                for i in 0..written {
+                    assert_eq!(read_buffer[size + i], bytes_new[i]);
+                }
             })
     }
 
+    /// ## Libfuzzer
+    ///
+    /// ```bash
+    /// cargo bolero test -p http_primitives mem::stream::validate::stream_iter
+    /// ```
+    ///
+    /// ## AFL
+    ///
+    /// ```bash
+    /// cargo bolero test -p http_primitives mem::stream::validate::stream_iter --engine afl --sanitizer NONE
+    /// ```
+    ///
+    /// ## Kani
+    ///
+    /// ```bash
+    /// cargo bolero test -p http_primitives mem::stream::validate::stream_iter --engine kani
+    /// ```
     #[test]
     #[cfg_attr(kani, kani::proof)]
     #[cfg_attr(kani, kani::unwind(17))]
@@ -471,29 +576,143 @@ mod validate {
 
         bolero::check!()
             .with_generator(generator)
-            .and_then(|(n, size)| {
+            .and_then(|(n, capacity)| {
                 (
                     n,
-                    size,
-                    // Stream start offset
-                    bolero::produce::<usize>().with().bounds(..size),
+                    capacity,
+                    // Initial stream start index
+                    bolero::produce::<usize>().with().bounds(..capacity),
+                    // Initial stream size
+                    bolero::produce::<usize>().with().bounds(..capacity),
                 )
             })
             .cloned()
-            .for_each(|(n, size, offset)| {
-                let bytes: [u8; MAX_SIZE] = std::array::from_fn(|i| i as u8);
+            .for_each(|(n, capacity, start, size)| {
                 let mut backing = [0; MAX_SIZE];
-                let mut stream = ByteStream::new_with_offset(&mut backing[..size], offset);
+                let bytes_new: [u8; MAX_SIZE] = std::array::from_fn(|i| i as u8);
+                let bytes_prev = backing;
 
-                let written = stream.write(&bytes[..n]).unwrap();
-                assert_eq!(written, n.min(size));
+                let mut stream = ByteStream::any(&mut backing[..capacity], start, size);
+
+                let written = stream.write(&bytes_new[..n]).unwrap();
+                assert_eq!(written, n.min(capacity - size));
 
                 let mut iter = stream.iter();
-                for n in &bytes[..written] {
-                    assert_eq!(Some(*n), iter.next());
+
+                // Make sure that previous data has not been overwritten
+                for i in 0..size {
+                    assert_eq!(iter.next(), Some(bytes_prev[(i + start) % capacity]));
+                }
+
+                // Check that new data has been written correctly
+                for i in 0..written {
+                    assert_eq!(iter.next(), Some(bytes_new[i]));
                 }
 
                 assert!(stream.is_empty());
             });
+    }
+}
+
+#[cfg(kani)]
+/// See [function contracts].
+///
+/// [function contracts]: https://model-checking.github.io/kani/crates/doc/kani/contracts/index.html
+mod contracts {
+    use super::*;
+
+    use std::io::Read as _;
+    use std::io::Write as _;
+
+    const MAX_SIZE: usize = 16;
+    const _: () = assert!(MAX_SIZE > 0);
+
+    /// General [`ByteStream`] pre-conditions, shared between [`Read`] and [`Write`] function
+    /// contracts.
+    ///
+    /// [`ByteStream`]: ByteStream
+    /// [`Read`]: std::io::Read
+    /// [`Write`]: std::io::Write
+    pub fn common_preconditions<'stream, 'data>(stream: &'stream mut ByteStream<'data>) -> bool {
+        !stream.buffer.is_empty()  // Stream buffer cannot have size 0
+            && stream.start < stream.capacity() // Start index must be less than stream capacity
+            && stream.size <= stream.capacity() // Stream size cannot exceed stream capacity
+    }
+
+    /// Contract validation tests MUST be run with `kani`.
+    ///
+    /// ```bash
+    /// cargo bolero test -p http_primitives mem::stream::contracts::check_read_contract --engine kani
+    /// ```
+    #[kani::proof_for_contract(<ByteStream as std::io::Read>::read)]
+    #[kani::unwind(17)]
+    fn check_read_contract() {
+        let capacity = kani::any();
+        let start = kani::any();
+        let size = kani::any();
+
+        kani::assume(capacity > 0 && capacity < MAX_SIZE);
+        kani::assume(start < capacity);
+        kani::assume(size < capacity);
+
+        let mut backing: [u8; MAX_SIZE] = std::array::from_fn(|i| i as u8);
+        let mut read_buffer = [0; MAX_SIZE];
+        let bytes_prev = backing;
+
+        let mut stream = ByteStream::any(&mut backing[..capacity], start, size);
+
+        let read = stream.read(&mut read_buffer).unwrap();
+
+        assert_eq!(read, size);
+        assert!(stream.is_empty());
+
+        for i in 0..read {
+            assert_eq!(read_buffer[i], bytes_prev[(i + start) % capacity]);
+        }
+    }
+
+    /// Contract validation tests MUST be run with `kani`.
+    ///
+    /// ```bash
+    /// cargo bolero test -p http_primitives mem::stream::contracts::check_write_contract --engine kani
+    /// ```
+    #[kani::proof_for_contract(<ByteStream as std::io::Write>::write)]
+    #[kani::unwind(17)]
+    fn check_write_contract() {
+        let n = kani::any();
+        let capacity = kani::any();
+        let start = kani::any();
+        let size = kani::any();
+
+        kani::assume(n < MAX_SIZE);
+        kani::assume(capacity > 0 && capacity < MAX_SIZE);
+        kani::assume(start < capacity);
+        kani::assume(size < capacity);
+
+        let mut backing: [u8; MAX_SIZE] = std::array::from_fn(|i| i as u8);
+        let mut read_buffer = [0; MAX_SIZE];
+
+        let bytes_prev = backing;
+        let bytes_new: [u8; MAX_SIZE] = std::array::from_fn(|i| (i + MAX_SIZE) as u8);
+
+        let mut stream = ByteStream::any(&mut backing[..capacity], start, size);
+
+        let written = stream.write(&bytes_new[..n]).unwrap();
+        assert_eq!(written, n.min(capacity - size));
+
+        let read = stream.read(&mut read_buffer).unwrap();
+
+        assert_eq!(read, written + size);
+        assert!(stream.is_empty());
+
+        // Make sure that previous data has not been overwritten
+        for i in 0..size {
+            assert_eq!(read_buffer[i], bytes_prev[(i + start) % capacity]);
+        }
+
+        // Check that new data has been written correctly
+        for i in 0..written {
+            assert_eq!(read_buffer[size + i], bytes_new[i]);
+        }
     }
 }
