6be32660f8bb2a6e3762339146a462f20f1b7416
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-08-21 20:18:45 +0200

    feat(nix): devShell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    kani-flake.url = "github:trantorian1/kani-flake";
+    kani-flake.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    nixpkgs,
+    kani-flake,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [kani-flake.overlays.default];
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        kani
+        cargo-bolero
+
+        (rust.override {extensions = ["rust-analyzer"];})
+      ];
+    };
+  };
+}
