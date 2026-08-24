bbebda063c47593f7259bb827f067de6411783ab
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-08-24 12:04:09 +0200

    deps(nix): update rust version

--- flake.nix
+++ flake.nix
@@ -4,17 +4,24 @@
 
     kani-flake.url = "github:trantorian1/kani-flake";
     kani-flake.inputs.nixpkgs.follows = "nixpkgs";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     nixpkgs,
     kani-flake,
+    rust-overlay,
     ...
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
-      overlays = [kani-flake.overlays.default];
+      overlays = [
+        kani-flake.overlays.default
+        rust-overlay.overlays.default
+      ];
     };
   in {
     devShells.${system}.default = pkgs.mkShell {
@@ -22,7 +29,13 @@
         kani
         cargo-bolero
 
-        (rust.override {extensions = ["rust-analyzer"];})
+        (rust-bin.selectLatestNightlyWith (toolchain:
+          toolchain.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+          }))
       ];
     };
   };
