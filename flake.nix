1d16d5e0ee5c8c391c33a9d386faa6a0a90e073c
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-08-25 11:15:39 +0200

    deps(nix): remove rust-overlay

--- flake.nix
+++ flake.nix
@@ -1,18 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
     kani-flake.url = "github:trantorian1/kani-flake";
-    kani-flake.inputs.nixpkgs.follows = "nixpkgs";
-
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     nixpkgs,
     kani-flake,
-    rust-overlay,
     ...
   }: let
     system = "x86_64-linux";
@@ -20,7 +14,6 @@
       inherit system;
       overlays = [
         kani-flake.overlays.default
-        rust-overlay.overlays.default
       ];
     };
   in {
@@ -29,13 +22,13 @@
         kani
         cargo-bolero
 
-        (rust-bin.selectLatestNightlyWith (toolchain:
-          toolchain.default.override {
-            extensions = [
-              "rust-src"
-              "rust-analyzer"
-            ];
-          }))
+        (rust-bin.override {
+          extensions = [
+            "rust-analyzer"
+
+            "rust-src"
+          ];
+        })
       ];
     };
   };
